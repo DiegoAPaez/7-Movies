20987413daaa3f2635a6f0f6ca3f2f77590759de
DiegoAPaez <diego.p31@hotmail.com>
2025-07-08 11:18:24 +0200

    Implemented Main view

---
+++ src/presentation/components/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import {Header} from "../organisms/Header.tsx";
+import {MoviesList} from "../organisms/MoviesList.tsx";
+
+export const MainPage = () => {
+    return (
+        <>
+            <Header/>
+            <MoviesList/>
+        </>
+    )
+}
