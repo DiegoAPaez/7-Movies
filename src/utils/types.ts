51080c63fb24a76ab4a084178db825703a5d6022
DiegoAPaez <diego.p31@hotmail.com>
2025-07-09 11:22:27 +0200

    Added details to Movie type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,22 @@
-export interface Movie {
-    id: string;
-    poster_path: string;
-    title: string;
-    release_date: string;
+export interface MoviesResponse {
+    page: number;
+    results: Movie[];
+    total_pages: number;
+    total_results: number;
 }
 
-export interface MoviesResponse {
-    results: Movie[];
+export interface Movie {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    backdrop_path: string;
+    release_date: string;
+    vote_average: number;
+    genres: Genre[];
+}
+
+interface Genre {
+    id: number;
+    name: string;
 }
