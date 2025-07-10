ae1379bcfc33f9515e546fc816a58f8f927b3ff9
DiegoAPaez <diego.p31@hotmail.com>
2025-07-10 10:47:21 +0200

    Implemented Routes and pages for loading more data

--- src/presentation/pages/HomePage.tsx
+++ src/presentation/pages/HomePage.tsx
@@ -5,7 +5,7 @@ export const HomePage = () => {
         <div>
             <h1>Welcome to the Movie App</h1>
             <p>Explore the latest movies and find your favorites!</p>
-            <Link to={"/movies"}>Browse Movies</Link>
+            <Link to={"/movies/"}>Browse Movies</Link>
         </div>
     );
 };
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,8 +1,9 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import { Header } from "../presentation/components/Header.tsx";
 import { HomePage } from "../presentation/pages/HomePage.tsx";
 import { MoviesPage } from "../presentation/pages/MoviesPage.tsx";
 import { MovieDetailPage } from "../presentation/pages/MovieDetailPage.tsx";
+import { Footer } from "../presentation/components/Footer.tsx";
 
 export const AppRouter = () => {
     return (
@@ -10,10 +11,15 @@ export const AppRouter = () => {
             <Header />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/movies" element={<MoviesPage />} />
+                <Route
+                    path="/movies"
+                    element={<Navigate to={"/movies/page/1"} />}
+                />
+                <Route path="/movies/page/:page" element={<MoviesPage />} />
                 <Route path="/movies/:id" element={<MovieDetailPage />} />
                 <Route path="*" element={<div>404 - Not Found</div>} />
             </Routes>
+            <Footer />
         </>
     );
 };
