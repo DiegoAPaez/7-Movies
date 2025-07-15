3346242bbfb74d037fe8f003b56948fc17e9c38f
DiegoAPaez <diego.p31@hotmail.com>
2025-07-15 11:18:40 +0200

    Added getPersonDetails and getMovieCredits for fetching data

--- src/data/Persons/persons.ts
+++ src/data/Persons/persons.ts
@@ -4,3 +4,13 @@ export const getCast = async (id: number) => {
     const response = await moviesApi.get(`/movie/${String(id)}/credits`);
     return response;
 };
+
+export const getPersonDetails = async (id: number) => {
+    const response = await moviesApi.get(`/person/${String(id)}`);
+    return response;
+};
+
+export const getMovieCredits = async (id: number) => {
+    const response = await moviesApi.get(`/person/${String(id)}/movie_credits`);
+    return response;
+};
